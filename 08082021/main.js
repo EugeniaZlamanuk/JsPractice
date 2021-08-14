942d4ff6e2bab8adb38cfb079f5680ce67fe12af
Zlamanuk <e.zlamanuk@gmail.com>
2021-08-15 00:44:35 +0300

    0308 and 0808

--- 08082021/main.js
+++ 08082021/main.js
@@ -1,9 +1,26 @@
-function buttonClick(){
+function buttonClick1(){
 
-    let y=0;
-    let x=2;
-    while (y<10){
-        alert (y+x);
-        y=y+x;
+    let n = 0;
+
+    while (n < 3) {
+        alert("номер "+n+"!")
+        n++;
     }
+
+
+
+}
+function buttonClick2(){
+    let ans;
+    do {
+        ans=prompt('Введите число больше 100');
+        if (ans<100) {
+            if (ans == null) {
+                break;
+            } else {
+                alert(ans + " меньше 100. Введите еще раз");
+            }
+        }
+    } while (ans <= 100);
+
 }
